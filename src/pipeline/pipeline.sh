239a8357f295d97ca1871f5eb354e1b6b637ff41
Josh Broomberg <joshbroomberg@gmail.com>
2020-10-27 17:01:07 -0700

    feature word2vec clean up

--- src/pipeline/pipeline.sh
+++ src/pipeline/pipeline.sh
@@ -6,5 +6,5 @@ python src/data/movie_list.py
 python src/data/overviews.py
 python src/data/cleaning_data.py
 python src/features/feature_eng.py
-sh src/models/get_word2vec.sh
+sh src/features/get_word2vec.sh
 python src/features/word2vec_features.py
